e2aed0a121e13104c44663930f7bb1cb73561dbd
edu292 <eduskoroboatei@gmail.com>
2025-06-01 19:23:15 -0300

    THis is the last time

--- Movie/settings.py
+++ Movie/settings.py
@@ -59,9 +59,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Movie.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default="sqlite:///db.sqlite3", conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
