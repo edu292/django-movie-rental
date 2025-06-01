9aaeeab6944c4e6c35c8d8488648fc8229b70674
edu292 <eduskoroboatei@gmail.com>
2025-06-01 18:03:52 -0300

    Starndard configs

--- Movie/settings.py
+++ Movie/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'Movie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'main/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
